3872079689973778bc89a59e8dc833e173dec1b7
wondersofcode <tunareyyublu0@gmail.com>
2025-11-21 07:39:18 +0000

    Task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 if __name__=="__main__":
-    a = 5
-    b = 10
+    a = 10
+    b = 5
     add = add(a, b)
     sub = sub(a, b)
     mul = mul(a, b)
